7086f8a126e8c6ecd95b9d95a31f846baa5d59e6
Dar Abarbanel <dar.abarbanel@gmail.com>
2025-06-28 12:58:35 +0300

    removed migration code after successful execution

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -49,12 +49,6 @@ func main() {
 	}
 	defer mongoDB.Close(context.Background())
 
-	// Run email field migration
-	logger.Info("Running email field migration...")
-	if err := database.MigrateRemoveEmailField(mongoDB.Database()); err != nil {
-		logger.WithError(err).Error("Failed to run email field migration")
-	}
-
 	// Create indexes
 	if err := mongoDB.CreateIndexes(context.Background()); err != nil {
 		logger.WithError(err).Error("Failed to create indexes")
